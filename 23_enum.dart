ca0191d5034c3230fcc93c4bede2beff814759f2
ihamedhadi <hamed.hadi1010@gmail.com>
2024-01-07 01:36:56 +0330

    Add enum file

---
+++ 23_enum.dart
@@ -0,0 +1,25 @@
+// Example 1: Basic Enumeration
+enum Color {
+  red,
+  green,
+  blue,
+}
+
+// Example 2: Enumeration with Associated Values
+enum Status<Map> {
+  success,
+  failure,
+  inProgress,
+}
+
+void main() {
+  // Example 1: Basic Enumeration
+  print('Example 1: Basic Enumeration');
+  Color selectedColor = Color.green;
+  print('Selected Color: $selectedColor');
+  print('Color Index: ${selectedColor.index}\n');
+  //Example 2: Enumeration with Associated Values
+  print('Example 2: Enumeration with Associated Values');
+  Status taskStatus = Status.inProgress;
+  print('Task Status: $taskStatus\n');
+}
