6d3b44ef5fd9d470191a941f08cc2007d04786f7
ihamedhadi <hamed.hadi1010@gmail.com>
2024-01-06 20:02:04 +0330

    Add if switch statement file

---
+++ 06_switch_statement.dart
@@ -0,0 +1,16 @@
+void main() {
+  // Example: Switch Statement
+  var num = 2 + 2;
+
+  switch (num) {
+    case 2:
+      print(2); // If num is 2, print 2
+      break;
+    case 3:
+      print(3); // If num is 3, print 3
+      break;
+    default:
+      print('default'); // If num doesn't match any case, print 'default'
+      break;
+  }
+}
